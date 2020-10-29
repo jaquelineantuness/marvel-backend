8692912b6a62fceceb11d8c1a349249d12d968b0
jaquelineantuness <antunes.jaqueline186@gmail.com>
2020-10-29 08:01:45 -0300

    Criando Get Character

--- src/main/java/com/marvel/api/controller/HomeController.java
+++ src/main/java/com/marvel/api/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.marvel.api.controller;
 
+import com.marvel.api.entity.Character;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HomeController {
     @GetMapping
-    public String home(){
-        return "Welcome Marvel API";
+    public Character home(){
+        Character character = new Character();
+
+        character.setName("Hulk2");
+//       character.setName("Iron Man");
+//        character.setDescription("aceedfew");
+        return character;
     }
 }
---
+++ src/main/java/com/marvel/api/entity/Character.java
@@ -0,0 +1,13 @@
+package com.marvel.api.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Character {
+    private String name;
+    private String description;
+
+
+}
