164f2e4acf21764cd9c979937c886f18c828def1
jaquelineantuness <antunes.jaqueline186@gmail.com>
2020-12-01 10:38:01 -0300

    Criação de massa de dados ao iniciar a aplicação, e start para criação de Swagger automatico

--- src/main/java/com/marvel/api/MarvelApiApplication.java
+++ src/main/java/com/marvel/api/MarvelApiApplication.java
@@ -1,13 +1,15 @@
 package com.marvel.api;
 
+import com.marvel.api.entity.Character;
+import com.marvel.api.repository.CharacterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MarvelApiApplication {
-
+public class MarvelApiApplication  extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(MarvelApiApplication.class, args);
 	}
-
 }
--- src/main/java/com/marvel/api/controller/CharacterController.java
+++ src/main/java/com/marvel/api/controller/CharacterController.java
@@ -6,6 +6,10 @@ import com.marvel.api.controller.dto.CharacterDto;
 import com.marvel.api.controller.form.CharacterForm;
 import com.marvel.api.entity.Character;
 import com.marvel.api.service.CharacterService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
@@ -20,11 +24,20 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/")
+@Api(value= "Characters")
 public class CharacterController {
 
     @Autowired
     CharacterService service;
 
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    @ApiOperation(value = "Listagem")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Retornou a lista com sucesso"),
+            @ApiResponse(code = 401, message = "Você não está autorizado a ver esse recurso"),
+            @ApiResponse(code = 403, message = "Acessar o recurso que você está tentando não é permitido"),
+            @ApiResponse(code = 404, message = "O recurso não foi encontrado")
+    })
     @GetMapping
     public ResponseEntity<Page<CharacterDto>> findAll(
             @PageableDefault(sort = "name",direction = Sort.Direction.ASC,page=0, size = 1)Pageable pageable){
